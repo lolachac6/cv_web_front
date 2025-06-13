7dac5bdca68035696a29ede7bda9121b157e40b8
Lola <lola_chacon8@hotmail.com>
2025-06-13 20:25:24 +0200

    Creación componente SkillComponent

---
+++ cv/src/app/components/skill-components/skill-components.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+
+@Component({
+  selector: 'app-skill-components',
+  imports: [MatProgressBarModule],
+  templateUrl: './skill-components.component.html',
+  styleUrl: './skill-components.component.css'
+})
+export class SkillComponentsComponent {
+
+}
