37f546d78a15a7567b53bfde17c25ebca089db23
Lola <lola_chacon8@hotmail.com>
2025-06-13 20:25:34 +0200

    Modificaciones de diseño

--- cv/src/app/components/home-components/home-components.component.ts
+++ cv/src/app/components/home-components/home-components.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { AboutMeComponentsComponent } from "../about-me-components/about-me-components.component";
 import { MyProyectComponentsComponent } from "../my-proyect-components/my-proyect-components.component";
 import { CardHomeComponent } from "../card-home/card-home.component";
+import { SkillComponentsComponent } from "../skill-components/skill-components.component";
 
 
 @Component({
   selector: 'app-home-components',
-  imports: [AboutMeComponentsComponent, MyProyectComponentsComponent, CardHomeComponent],
+  imports: [AboutMeComponentsComponent, MyProyectComponentsComponent, CardHomeComponent, SkillComponentsComponent],
   templateUrl: './home-components.component.html',
   styleUrls: ['./home-components.component.css']
 })
