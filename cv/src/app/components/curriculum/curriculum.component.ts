d8152650ae144515e5158e2526d3367858a16156
Lola <lola_chacon8@hotmail.com>
2025-06-17 01:03:59 +0200

    creacion componente curriculum

---
+++ cv/src/app/components/curriculum/curriculum.component.ts
@@ -0,0 +1,15 @@
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-curriculum',
+  imports: [],
+  templateUrl: './curriculum.component.html',
+  styleUrl: './curriculum.component.css'
+})
+export class CurriculumComponent {
+   
+
+
+
+}
