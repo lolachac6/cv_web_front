a48ab2eac5dff24a0e3bba8233b8597cfdb7b0f0
Lola <lola_chacon8@hotmail.com>
2025-06-16 15:40:11 +0200

    Creación de componente language y modificaciones  css

---
+++ cv/src/app/components/language-component/language-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
+@Component({
+  selector: 'app-language-component',
+  imports: [MatProgressBarModule],
+  templateUrl: './language-component.component.html',
+  styleUrl: './language-component.component.css'
+})
+export class LanguageComponentComponent {
+
+}
--- cv/src/app/components/skill-components/skill-components.component.ts
+++ cv/src/app/components/skill-components/skill-components.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { LanguageComponentComponent } from "../language-component/language-component.component";
 
 @Component({
   selector: 'app-skill-components',
-  imports: [MatProgressBarModule],
+  imports: [MatProgressBarModule, LanguageComponentComponent],
   templateUrl: './skill-components.component.html',
   styleUrl: './skill-components.component.css'
 })
