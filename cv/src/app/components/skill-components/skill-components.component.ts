03828e230c22a393658ff61559b3a40ee89ffc5c
Lola <lola_chacon8@hotmail.com>
2025-06-16 19:11:30 +0200

    modificaciones en el componente skill

--- cv/src/app/components/skill-components/skill-components.component.ts
+++ cv/src/app/components/skill-components/skill-components.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import { LanguageComponentComponent } from "../language-component/language-component.component";
+import { AbilitiesComponentComponent } from "../abilities-component/abilities-component.component";
 
 @Component({
   selector: 'app-skill-components',
-  imports: [MatProgressBarModule, LanguageComponentComponent],
+  imports: [ LanguageComponentComponent, AbilitiesComponentComponent],
   templateUrl: './skill-components.component.html',
   styleUrl: './skill-components.component.css'
 })
