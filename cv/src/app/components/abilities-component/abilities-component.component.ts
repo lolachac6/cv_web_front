a664269d66b585cab20f01cbab43636955bd1514
Lola <lola_chacon8@hotmail.com>
2025-06-16 19:11:11 +0200

    creación del componente abilities

---
+++ cv/src/app/components/abilities-component/abilities-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+
+@Component({
+  selector: 'app-abilities-component',
+  imports: [MatProgressSpinner ],
+  templateUrl: './abilities-component.component.html',
+  styleUrl: './abilities-component.component.css'
+})
+export class AbilitiesComponentComponent {
+
+}
