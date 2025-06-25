944aa29b2f6214a0d7f52c0adec4fed4a3632d11
Lola <lola_chacon8@hotmail.com>
2025-06-25 18:39:36 +0200

    creación de componente education-web

---
+++ cv/src/app/components/education-web/education-web.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-education-web',
+  imports: [],
+  templateUrl: './education-web.component.html',
+  styleUrl: './education-web.component.css'
+})
+export class EducationWebComponent {
+
+}
