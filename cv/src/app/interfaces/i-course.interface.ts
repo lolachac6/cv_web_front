c999f443ef1094e5e21ff88ca2405a919e10bcb3
Lola <lola_chacon8@hotmail.com>
2025-06-26 18:09:42 +0200

    creación de interface course

---
+++ cv/src/app/interfaces/i-course.interface.ts
@@ -0,0 +1,10 @@
+export interface ICourse {
+    id: number, 
+    title: string, 
+    dates: Date | String,
+    datesEnd: Date | String, 
+    hours: number,
+    image:string,
+    Skill: [String]
+
+}
