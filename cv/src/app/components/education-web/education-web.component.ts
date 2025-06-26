31320f7daedd450828b1420fcf7d1d1c3dba93a3
Lola <lola_chacon8@hotmail.com>
2025-06-26 18:10:45 +0200

    maquetación, inyección de courseServices

--- cv/src/app/components/education-web/education-web.component.ts
+++ cv/src/app/components/education-web/education-web.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { CoursesService } from '../../services/courses.service';
+import { ICourse } from '../../interfaces/i-course.interface';
 
 @Component({
   selector: 'app-education-web',
-  imports: [],
+  imports: [MatGridListModule],
   templateUrl: './education-web.component.html',
   styleUrl: './education-web.component.css'
 })
 export class EducationWebComponent {
 
+  coursesService = inject(CoursesService);
+  arrCoursesWeb: ICourse[]= [];
+
+
+  async ngOnInit(){
+    this.arrCoursesWeb = await this.coursesService.getAllExperience();
+  }
+
+  apareceElTitulo(url:string,id:Number){
+    let ids = String(id);
+    const x = document.getElementById(ids);
+    let img = document.querySelector('.elemento-invisible');
+    if(!img){
+    let image = document.createElement('img');
+    image.setAttribute('src',url);
+    image.className ='elemento-invisible'
+    image.style.width = '600px';
+    image.style.height = 'auto';
+      x?.appendChild(image);
+    }else{
+      img.remove();
+    }
+    
+      
+  }
+  
 }
